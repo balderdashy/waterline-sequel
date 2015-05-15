e60dc783f6a1e577e5705bdc1c8a5f406118cf79
anotherpit <anotherpit@gmail.com>
2015-05-15 14:24:18 +0300

    Add tests for sort clause

---
+++ test/queries/simpleSelectSort.js
@@ -0,0 +1,40 @@
+module.exports = {
+
+  // A description. This is used for display in the test output.
+  description: 'Should construct a simple select query with a sort clause',
+
+  // The name of the table this query should be ran against.
+  table      : 'foo',
+
+  // The query object used to build this query.
+  query      : {
+    where: null,
+    sort: {
+      color: 1
+    }
+  },
+
+  // Expected results per query method.
+  expected   : {
+
+    // Sequel.select()
+    select: {
+
+      // The queryString we expect to be rendered after calling `Sequel.select()`
+      queryString    : 'SELECT `foo`.`color`, `foo`.`id`, `foo`.`createdAt`, `foo`.`updatedAt`, `foo`.`bar`, `foo`.`bat`, `foo`.`baz` FROM `foo` AS `foo` ',
+
+      // The number of queries that will be returned after calling Sequel.select()
+      queriesReturned: 1
+    },
+
+    // Sequel.find()
+    find  : {
+
+      // The queryString we expect to be rendered after calling `Sequel.select()`
+      queryString    : 'SELECT `foo`.`color`, `foo`.`id`, `foo`.`createdAt`, `foo`.`updatedAt`, `foo`.`bar`, `foo`.`bat`, `foo`.`baz` FROM `foo` AS `foo`   ORDER BY `foo`.`color` ASC',
+
+      // The number of queries that will be returned after calling Sequel.select()
+      queriesReturned: 1
+    }
+  }
+};

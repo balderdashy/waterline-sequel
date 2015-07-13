5ca0979e608b9fcb69b8e0dfa43fa89866fe7bc5
Cody Stoltman <particlebanana@gmail.com>
2015-07-13 18:01:08 -0500

    Fix to make count query support Limit and Offset

--- sequel/index.js
+++ sequel/index.js
@@ -119,8 +119,9 @@ Sequel.prototype.count = function count(currentTable, queryObject) {
 
   // Step 1:
   // Build out the Count statements
-  // TO-DO: limit this to a certain column, e.g. id, for performance gains
-  this.queries = ['SELECT COUNT(*) FROM ' + tableName];
+  this.queries = ['SELECT COUNT(*) as count FROM '];
+
+  var subQuery = 'SELECT * FROM ' + tableName;
 
   var whereObject;
   var childQueries;
@@ -128,20 +129,15 @@ Sequel.prototype.count = function count(currentTable, queryObject) {
   var values;
 
   /**
-   * Step 2 - Build out the parent query.
+   * Step 2 - Build out the WHERE part of the query.
    */
 
   whereObject = this.simpleWhere(currentTable, queryObject);
 
-  this.queries[0] += ' ' + whereObject.query;
+  // Append the sub-query to the COUNT so you end up with something that looks like:
+  // SELECT count(*) as count FROM (SELECT * FROM table LIMIT 10 OFFSET 10) AS tableAlias;
+  this.queries[0] += '(SELECT * FROM ' + tableName + ' ' + whereObject.query + ') AS ' + tableName;
   this.values[0] = whereObject.values;
-
-  /**
-   * Step 3 - Build out the child query templates.
-   */
-
-  childQueries = this.complexWhere(currentTable, queryObject);
-  this.queries = this.queries.concat(childQueries);
 
   return {
     query: this.queries,

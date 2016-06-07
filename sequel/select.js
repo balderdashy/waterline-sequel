124bc2a3d269c9c5b72da2da2e82417bcc51e667
Cody Stoltman <particlebanana@gmail.com>
2016-06-07 15:49:45 -0500

    use a * so empty selects don't end up with SELEC FROM

--- sequel/select.js
+++ sequel/select.js
@@ -126,8 +126,15 @@ SelectBuilder.prototype.buildSimpleSelect = function buildSimpleSelect(queryObje
       query += utils.escapeName(select.table, self.escapeCharacter) + '.' + utils.escapeName(select.key, self.escapeCharacter) + ', ';
     }
   });
-  // Remove the last comma
-  query = query.slice(0, -2) + ' FROM ' + tableName + ' AS ' + utils.escapeName(self.currentTable, self.escapeCharacter) + ' ';
+
+  // Remove the last comma in a list of items being selected
+  if(selectKeys.length) {
+    query = query.slice(0, -2);
+  } else {
+    query += '*';
+  }
+
+  query = query + ' FROM ' + tableName + ' AS ' + utils.escapeName(self.currentTable, self.escapeCharacter) + ' ';
   return query;
 };
 
